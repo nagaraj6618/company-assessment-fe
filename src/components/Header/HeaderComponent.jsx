2932e5c474c4512cb586b5c75796ba1bae8a643a
NAGARAJ S <nagaraj516700@gmail.com>
2024-05-01 19:34:42 +0530

    Fix : Header bg color changed

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -12,7 +12,7 @@ const HeaderComponent = () => {
 
   return (
     <>
-      <div className='bg-[#1c1c84] text-white flex justify-between items-center p-2'>
+      <div className='bg-slate-800 text-white flex justify-between items-center p-2'>
         <div className='hidden md:flex space-x-4 pt-2'>
           <Link to='/' className='pl-5 hover:text-gray-300 transition duration-300'>Home</Link>
           <Link to='/post' className='pl-5 hover:text-gray-300 transition duration-300'>Post</Link>
@@ -23,7 +23,7 @@ const HeaderComponent = () => {
           {!isOpen ? <IoMdMenu onClick={toggleEvent} className='text-xl cursor-pointer pt-1 hover:text-gray-300 transition duration-300' /> : <IoCloseSharp onClick={toggleEvent} className='text-xl cursor-pointer hover:text-gray-300 transition duration-300' />}
         </div>
       </div>
-      <div className={`bg-[#1c1c84] text-white p-2 transition-all duration-300 ${isOpen ? 'h-auto' : 'h-0 overflow-hidden'}`}>
+      <div className={`bg-slate-800 text-white p-2 transition-all duration-300 ${isOpen ? 'h-auto' : 'h-0 overflow-hidden'}`}>
         <Link to='/' className='block pl-5 py-2 hover:text-gray-300 transition duration-300'>Home</Link>
         <Link to='/post' className='block pl-5 py-2 hover:text-gray-300 transition duration-300'>Post</Link>
         <Link to='/login' className='block pl-5 py-2 hover:text-gray-300 transition duration-300'>Login</Link>
