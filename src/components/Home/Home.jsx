4c5ec51c0eaa13773e9a31d8adeef466f9b3a77c
NAGARAJ S <nagaraj516700@gmail.com>
2024-05-02 18:27:22 +0530

    Feat : Logout button Added in the Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,46 +2,57 @@ import React, { useEffect, useState } from 'react'
 import ErrorDisplay from '../MessageDisplay/ErrorDisplay'
 import { Link } from 'react-router-dom';
 const Home = () => {
-  const [userData , setUserData] = useState(
+  const [userData, setUserData] = useState(
     {
-      userName:'',
-      name:''
+      userName: '',
+      name: ''
     }
   );
-  const [error ,setError] = useState(null);
+  const [error, setError] = useState(null);
   useEffect(() => {
-    
-    setUserData({
-      userName:localStorage.getItem('username'),
-      name:localStorage.getItem('name'),
-      })
-   
-      setError('Please Login!')
-    
 
-    } , [])
+    setUserData({
+      userName: localStorage.getItem('username'),
+      name: localStorage.getItem('name'),
+    })
+
+    setError('Please Login!')
+  }, [])
+
+  const logoutHandler = () => {
+    localStorage.clear();
+    sessionStorage.clear();
+    window.location.href = '/'
+  }
   return (
     <div>
-    {userData.userName && userData.name&& (
-    <div className='flex h-screen justify-center items-center text-white'>
-    <div className='dark:bg-slate-800 p-10 rounded shadow'>
-      <div className='p-2'>
-        <p className='text-lg font-semibold'>Name: <span className='text-blue-500'>{userData.name}</span></p>
-      </div>
-      <div className='p-2'>
-        <p className='text-lg font-semibold'>Username: <span className='text-blue-500'>{userData.userName}</span></p>
-      </div>
+      {userData.userName && userData.name && (
+        <div className='flex h-screen justify-center items-center text-white'>
+          <div className='dark:bg-slate-800 p-10 rounded shadow'>
+            <div className='p-2'>
+              <p className='text-lg font-semibold'>Name: <span className='text-blue-500'>{userData.name}</span></p>
+            </div>
+            <div className='p-2'>
+              <p className='text-lg font-semibold'>Username: <span className='text-blue-500'>{userData.userName}</span></p>
+            </div>
+            <div className='p-2'>
+            
+            <button className='block bg-white text-slate-800 px-2 py-1 rounded cursor-pointer hover:text-slate-800 transition duration-300  ' onClick={logoutHandler}>
+              Logout
+            </button>
+          </div>
+          </div>
+          
+        </div>
+      )}
+      {
+        !userData.name && !userData.userName && (
+          <div>
+            <ErrorDisplay error={error} setError={setError}></ErrorDisplay>
+            <p>Please Login <Link to='/login' className='text-blue-500'>Click here.</Link></p>
+          </div>)
+      }
     </div>
-    </div>
-    )}
-    {
-      !userData.name && !userData.userName && (
-      <div>
-        <ErrorDisplay error={error} setError={setError}></ErrorDisplay>
-        <p>Please Login <Link to='/login' className='text-blue-500'>Click here.</Link></p>
-      </div>)
-    }
-  </div>
   )
 }
 
